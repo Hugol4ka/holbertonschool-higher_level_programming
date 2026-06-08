f14f38eba56546d8f68f8864bc30d47827ded803
Hugol4ka <hugolaca12300@gmail.com>
2026-06-08 18:18:29 +0200

    Fix code for pycodestyle

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+"""lists all State objects that contain
+the letter 'a' from the database hbtn_0e_6_usa"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -12,7 +13,8 @@ if __name__ == "__main__":
     engine = create_engine(db_url, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
     for state in states:
         print(f"{state.id}: {state.name}")
     session.close()
