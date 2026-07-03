4884bc6154d13a069d903f726d7553e57f146bf9
Hugol4ka <hugolaca12300@gmail.com>
2026-07-03 12:05:46 +0200

    Add items page with JSON data and corresponding route

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        items_data = json.load(file)
+        rendered_html = render_template('items.html', items=items_data['items'])
+    return rendered_html
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
